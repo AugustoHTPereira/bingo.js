3633fcffb030c4e5dd7a2ed5c46315138acc5c5e
Augusto Pereira <augustohtp8@gmail.com>
2020-01-17 16:52:50 -0300

    Remove data

--- script.js
+++ script.js
@@ -7,6 +7,12 @@ init = () => {
     column: "",
     number: 0
   };
+  for (var i = 1; i < 16; i++) {
+    var row = mainTable.insertRow(i);
+    for (var a = 0; a < 5; a++) {
+      var cell = row.insertCell(a);
+    }
+  }
 };
 
 push = event => {
@@ -31,6 +37,8 @@ push = event => {
 
 pull = number => {
   this.numbers = this.numbers.filter(el => el.number != number);
+  let bounds = this.mapNumBounds({ number: number });
+  this.mainTable.rows[bounds.x].cells[bounds.y].innerText = "";
   this.render();
 };
 
@@ -57,6 +65,7 @@ render = () => {
   for (let i = this.numbers.length - 1; i >= 0; i--) {
     console.log(this.numbers[i]);
     appendManagerTable(this.numbers[i]);
+    appendMainTable(this.numbers[i]);
   }
 };
 
@@ -95,10 +104,22 @@ appendManagerTable = el => {
   this.alldigits.appendChild(newRow);
 };
 
-appendMainTable = el => {};
+appendMainTable = last => {
+  let bounds = mapNumBounds(last);
+  this.mainTable.rows[bounds.x].cells[bounds.y].innerText = last.number;
+};
 
 arrayHas = (arr, key) => {
   for (let i = 0; i < arr.length; i++)
     if (arr[i].column == key.column && arr[i].number == key.number) return true;
   return false;
+};
+
+mapNumBounds = last => {
+  let num = parseInt(last.number);
+  if (num <= 15) return { x: num, y: 0 };
+  if (num <= 30) return { x: num - 15, y: 1 };
+  if (num <= 45) return { x: num - 30, y: 2 };
+  if (num <= 60) return { x: num - 45, y: 3 };
+  if (num <= 75) return { x: num - 60, y: 4 };
 };
